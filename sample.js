d14ff8a9fae5a9dd092515b103ded486a95ab44c
Vish2007 <iamvishsurya@gmail.com>
2024-08-14 03:01:22 +0000

    add package.json with type module to fix js module error while exicuting get pokemon data files

--- sample.js
+++ sample.js
@@ -3,12 +3,14 @@ import { base, fusions, triples } from "./lib/loadPokemon.js";
 // Log all base pokemon, fusions, and triples
 for (const pokemon of base) {
 	console.log(pokemon);
+
+	if (pokemon.id >= 5) break;
 }
 
-for (const pokemon of fusions) {
-	console.log(pokemon.id, pokemon.name);
-}
+// for (const pokemon of fusions) {
+// 	console.log(pokemon.id, pokemon.name);
+// }
 
-for (const pokemon of triples) {
-	console.log(pokemon.id, pokemon.name);
-}
+// for (const pokemon of triples) {
+// 	console.log(pokemon.id, pokemon.name);
+// }
